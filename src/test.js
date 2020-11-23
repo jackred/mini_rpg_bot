299e4abc3aea82feae418e2eb9cf7b62850dbf96
Jackred <jackred@tuta.io>
2020-11-23 00:22:38 +0000

    UPDATE testing files

--- src/test.js
+++ src/test.js
@@ -3,18 +3,61 @@ const Fighter = require('./Class/Fighter');
 const Elf = require('./Race/Elf');
 const { getDefaultStats } = require('./Stats');
 const { displayEntity } = require('./Display/Display');
+const Equipement = require('./Equipement/Equipement');
 
-const Discord = require('discord.js');
-const config = require('../config.json');
-
-const client = new Discord.Client({
-  partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
+let sword = new Equipement({
+  bonus: { str: 7, atk: 8, bba: 4 },
+  name: 'Sword of Albion',
+  size: 'M',
+  type: 'weapon',
+  equipementType: 'main',
+  userType: 'humanoid',
 });
-client.on('ready', () => {
-  console.log('Starting!');
+
+let knife = new Equipement({
+  bonus: { atk: 2 },
+  name: 'Kitchen knife',
+  size: 'M',
+  type: 'weapon',
+  equipementType: 'second',
+  userType: 'humanoid',
 });
 
-client.login(config.token);
+let bow = new Equipement({
+  bonus: { atk: 6, crit: 0.1 },
+  name: 'Long bow of the ardent flame',
+  size: 'M',
+  type: 'weapon',
+  equipementType: 'range',
+  userType: 'humanoid',
+});
+
+let shield = new Equipement({
+  bonus: { def: 4 },
+  name: 'Sunrise shield',
+  size: 'M',
+  type: 'armor',
+  equipementType: 'shield',
+  userType: 'humanoid',
+});
 
 let gobo = new Goblin(1, 'Gobo');
-let guard = new Fighter(1, Elf, 'Jonh', getDefaultStats());
+let guard = new Fighter(1, 'Jonh', new Elf(), {
+  stats: getDefaultStats(),
+  equipements: [sword, shield, knife, bow],
+});
+
+console.log(gobo);
+console.log(guard);
+
+// const Discord = require('discord.js');
+// const config = require('../config.json');
+
+// const client = new Discord.Client({
+//   partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
+// });
+// client.on('ready', () => {
+//   console.log('Starting!');
+// });
+
+// client.login(config.token);
